47966486415c32097b7c8fc5a354e94cc32adbea
Yoga Darma <kadekyoga125@gmail.com>
2021-03-21 01:29:48 +0700

    add shimmer for loading

--- app/src/main/java/com/yogadarma/stockbitapp/presentation/watchlist/WatchlistFragment.kt
+++ app/src/main/java/com/yogadarma/stockbitapp/presentation/watchlist/WatchlistFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yogadarma.core.data.Resource
 import com.yogadarma.stockbitapp.databinding.FragmentWatchlistBinding
+import com.yogadarma.stockbitapp.utils.toast
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class WatchlistFragment : Fragment() {
@@ -33,13 +36,14 @@ class WatchlistFragment : Fragment() {
 
         watchlistViewModel.getAllCrypto(50, "USD").observe(viewLifecycleOwner, { response ->
             when (response) {
-                is Resource.Loading -> {
-                }
+                is Resource.Loading -> showShimmer()
                 is Resource.Success -> {
+                    dismissShimmer()
                     watchlistAdapter.setData(response.data)
                 }
                 is Resource.Error -> {
-
+                    dismissShimmer()
+                    context?.toast(response.message.toString())
                 }
             }
         })
@@ -47,8 +51,17 @@ class WatchlistFragment : Fragment() {
         with(binding.rvCrypto) {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
             adapter = watchlistAdapter
         }
+    }
+
+    private fun showShimmer() {
+        binding.svShimmer.visibility = View.VISIBLE
+    }
+
+    private fun dismissShimmer() {
+        binding.svShimmer.visibility = View.GONE
     }
 
     override fun onDestroyView() {
