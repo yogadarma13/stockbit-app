3d77e683a008011643d41be91b0124dbc9005a35
Yoga Darma <kadekyoga125@gmail.com>
2021-03-22 15:06:46 +0700

    remove unused resources

--- core/src/main/java/com/yogadarma/core/data/source/remote/response/ListCryptoResponse.kt
+++ core/src/main/java/com/yogadarma/core/data/source/remote/response/ListCryptoResponse.kt
@@ -4,48 +4,48 @@ import com.google.gson.annotations.SerializedName
 
 data class ListCryptoResponse(
 
-	@field:SerializedName("Data")
-	val data: List<DataItem>? = null,
+    @field:SerializedName("Data")
+    val data: List<DataItem>? = null
 
-	)
+)
 
 data class CoinInfo(
 
-	@field:SerializedName("Name")
-	val name: String? = null,
+    @field:SerializedName("Name")
+    val name: String? = null,
 
-	@field:SerializedName("FullName")
-	val fullName: String? = null,
+    @field:SerializedName("FullName")
+    val fullName: String? = null,
 
-	@field:SerializedName("Id")
-	val id: String? = null,
+    @field:SerializedName("Id")
+    val id: String? = null
 
-	)
+)
 
 data class Usd(
 
-	@field:SerializedName("IMAGEURL")
-	val imageUrl: String? = null,
+    @field:SerializedName("IMAGEURL")
+    val imageUrl: String? = null,
 
-	@field:SerializedName("PRICE")
-	val price: String? = null,
+    @field:SerializedName("PRICE")
+    val price: String? = null,
 
-	@field:SerializedName("CHANGEPCT24HOUR")
-	val changePct24Hour: String? = null,
+    @field:SerializedName("CHANGEPCT24HOUR")
+    val changePct24Hour: String? = null
 
-	)
+)
 
 data class Display(
 
-	@field:SerializedName("USD")
-	val usd: Usd? = null
+    @field:SerializedName("USD")
+    val usd: Usd? = null
 )
 
 data class DataItem(
 
-	@field:SerializedName("DISPLAY")
-	val display: Display? = null,
+    @field:SerializedName("DISPLAY")
+    val display: Display? = null,
 
-	@field:SerializedName("CoinInfo")
-	val coinInfo: CoinInfo? = null
+    @field:SerializedName("CoinInfo")
+    val coinInfo: CoinInfo? = null
 )
